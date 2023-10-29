516936bdb02b236a1a0be37fa1b24c39fabb5f10
TylerNeverlived <107303031+TylerNeverlived@users.noreply.github.com>
2023-10-30 00:07:32 +0200

    some change

--- src/mob-menu.js
+++ src/mob-menu.js
@@ -1,19 +1,32 @@
-(() => {
- const refs = {
-  openMenuBtn: document.querySelector(“[data-menuBTN-open]“),
-  closeMenuBtn: document.querySelector(“[data-menuBTN-close]“),
-  menu: document.querySelector(“[data-menu]“),
-  body: document.querySelector(“header”),
- };
- refs.openMenuBtn.addEventListener(“click”, toggleModal);
- refs.closeMenuBtn.addEventListener(“click”, toggleModal);
- function toggleModal() {
-  refs.menu.classList.toggle(“is-open”);
-  refs.body.classList.toggle(“no-scroll”);
- }
- window.matchMedia(“(min-width: 768px)“).addEventListener(“change”, (e) => {
-  if (!e.matches) return;
-  refs.menu.classList.remove(“is-open”);
-  refs.body.classList.remove(“no-scroll”);
- });
-})();
+'use strict';
+
+const mobMenuWindow = document.querySelector('.mob-menu');
+const overlay = document.querySelector('.backdrop');
+const btnCloseMobMenuWindow = document.querySelector('.close-mob-btn');
+const btnsShowMobMenuWindow = document.querySelectorAll('.menu-open-btn');
+console.log(btnsShowMobMenuWindow);
+
+const showMobMenuWindow = function () {
+  mobMenuWindow.classList.add('is-open');
+  //  modalWindow.style.display = 'block';
+  overlay.classList.add('is-open');
+};
+
+const closeMobMenuWindow = function () {
+  mobMenuWindow.classList.remove('is-open');
+  overlay.classList.remove('is-open');
+};
+
+for (let i = 0; i < btnsShowMobMenuWindow.length; i++) {
+  btnsShowMobMenuWindow[i].addEventListener('click', showMobMenuWindow);
+}
+
+btnCloseMobMenuWindow.addEventListener('click', closeMobMenuWindow);
+
+overlay.addEventListener('click', closeMobMenuWindow);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !mobMenuWindow.classList.contains('is-open')) {
+    closeMobMenuWindow();
+  }
+});
