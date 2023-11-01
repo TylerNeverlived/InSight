ee9a3260333722edb7f6f3ae98c41decbd3eb517
TylerNeverlived <107303031+TylerNeverlived@users.noreply.github.com>
2023-11-01 20:32:15 +0200

    js

--- src/mob-menu.js
+++ src/mob-menu.js
@@ -1,32 +1,32 @@
-'use strict';
+// 'use strict';
 
-const mobMenuWindow = document.querySelector('.mob-menu');
-const overlay = document.querySelector('.backdrop');
-const btnCloseMobMenuWindow = document.querySelector('.close-mob-btn');
-const btnsShowMobMenuWindow = document.querySelectorAll('.menu-open-btn');
-console.log(btnsShowMobMenuWindow);
+// const mobMenuWindow = document.querySelector('.mob-menu');
+// const overlay = document.querySelector('.backdrop');
+// const btnCloseMobMenuWindow = document.querySelector('.close-mob-btn');
+// const btnsShowMobMenuWindow = document.querySelectorAll('.menu-open-btn');
+// console.log(btnsShowMobMenuWindow);
 
-const showMobMenuWindow = function () {
-  mobMenuWindow.classList.add('is-open');
-  //  modalWindow.style.display = 'block';
-  overlay.classList.add('is-open');
-};
+// const showMobMenuWindow = function () {
+//   mobMenuWindow.classList.add('is-open');
+//   //  modalWindow.style.display = 'block';
+//   overlay.classList.add('is-open');
+// };
 
-const closeMobMenuWindow = function () {
-  mobMenuWindow.classList.remove('is-open');
-  overlay.classList.remove('is-open');
-};
+// const closeMobMenuWindow = function () {
+//   mobMenuWindow.classList.remove('is-open');
+//   overlay.classList.remove('is-open');
+// };
 
-for (let i = 0; i < btnsShowMobMenuWindow.length; i++) {
-  btnsShowMobMenuWindow[i].addEventListener('click', showMobMenuWindow);
-}
+// for (let i = 0; i < btnsShowMobMenuWindow.length; i++) {
+//   btnsShowMobMenuWindow[i].addEventListener('click', showMobMenuWindow);
+// }
 
-btnCloseMobMenuWindow.addEventListener('click', closeMobMenuWindow);
+// btnCloseMobMenuWindow.addEventListener('click', closeMobMenuWindow);
 
-overlay.addEventListener('click', closeMobMenuWindow);
+// overlay.addEventListener('click', closeMobMenuWindow);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !mobMenuWindow.classList.remove('is-open')) {
-    closeMobMenuWindow();
-  }
-});
+// document.addEventListener('keydown', function (e) {
+//   if (e.key === 'Escape' && !mobMenuWindow.classList.remove('is-open')) {
+//     closeMobMenuWindow();
+//   }
+// });
--- src/test.js
+++ src/test.js
@@ -1,50 +1,50 @@
-const mediaQuery = window.matchMedia(
-  '(min-width: 320px) and (max-width: 767px)'
-);
-const overlay = document.querySelector('.backdrop');
-const mobMenuWindow = document.querySelector('.mob-menu');
+// const mediaQuery = window.matchMedia(
+//   '(min-width: 320px) and (max-width: 767px)'
+// );
+// const overlay = document.querySelector('.backdrop');
+// const mobMenuWindow = document.querySelector('.mob-menu');
 
-function handleTabletMediaQuery(event) {
-  const mobileMenu = document.getElementById('mobile-menu');
+// function handleTabletMediaQuery(event) {
+//   const mobileMenu = document.getElementById('mobile-menu');
 
-  //   if (event.matches) {
-  // Mobile view: screen width between 320px and 767px
-  document.getElementById('menu-button').addEventListener('click', function () {
-    mobileMenu.style.right = '0';
-    overlay.classList.add('is-open');
-    mobMenuWindow.classList.add('is-open');
-  });
+//   //   if (event.matches) {
+//   // Mobile view: screen width between 320px and 767px
+//   document.getElementById('menu-button').addEventListener('click', function () {
+//     mobileMenu.style.right = '0';
+//     overlay.classList.add('is-open');
+//     mobMenuWindow.classList.add('is-open');
+//   });
 
-  document
-    .getElementById('close-button')
-    .addEventListener('click', function () {
-      mobileMenu.style.right = '-380px';
-      overlay.classList.remove('is-open');
-      //   mobMenuWindow.classList.remove('is-open');
-    });
+//   document
+//     .getElementById('close-button')
+//     .addEventListener('click', function () {
+//       mobileMenu.style.right = '-380px';
+//       overlay.classList.remove('is-open');
+//       //   mobMenuWindow.classList.remove('is-open');
+//     });
 
-  overlay.addEventListener('click', function () {
-    mobileMenu.style.right = '-380px';
-    // mobMenuWindow.classList.remove('is-open');
-    overlay.classList.remove('is-open');
-  });
+//   overlay.addEventListener('click', function () {
+//     mobileMenu.style.right = '-380px';
+//     // mobMenuWindow.classList.remove('is-open');
+//     overlay.classList.remove('is-open');
+//   });
 
-  //   overlay.addEventListener('click', closeMobMenuWindow);
-  //   } else {
-  //     // Tablet view: screen width greater than or equal to 768px
-  //     document
-  //       .getElementById('menu-button')
-  //       .addEventListener('click', function () {
-  //         mobileMenu.style.right = '0';
-  //       });
+//   //   overlay.addEventListener('click', closeMobMenuWindow);
+//   //   } else {
+//   //     // Tablet view: screen width greater than or equal to 768px
+//   //     document
+//   //       .getElementById('menu-button')
+//   //       .addEventListener('click', function () {
+//   //         mobileMenu.style.right = '0';
+//   //       });
 
-  //     document
-  //       .getElementById('close-button')
-  //       .addEventListener('click', function () {
-  //         mobileMenu.style.right = '-380px';
-  //       });
-  //   }
-}
+//   //     document
+//   //       .getElementById('close-button')
+//   //       .addEventListener('click', function () {
+//   //         mobileMenu.style.right = '-380px';
+//   //       });
+//   //   }
+// }
 
-mediaQuery.addListener(handleTabletMediaQuery); // Attach the event listener
-handleTabletMediaQuery(mediaQuery); // Call the function once to check the initial state
+// mediaQuery.addListener(handleTabletMediaQuery); // Attach the event listener
+// handleTabletMediaQuery(mediaQuery); // Call the function once to check the initial state
