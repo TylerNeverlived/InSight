5c1f4004511cbbe88f0147172f7d403385fe6c24
TylerNeverlived <107303031+TylerNeverlived@users.noreply.github.com>
2023-11-02 22:09:46 +0200

    add close

--- src/main.js
+++ src/main.js
@@ -4,11 +4,20 @@ const mediaQuery = window.matchMedia(
 const overlay = document.querySelector('.backdrop');
 const mobMenuWindow = document.querySelector('.mob-menu');
 
+function closeMobileMenu() {
+  const mobileMenu = document.getElementById('mobile-menu');
+  mobileMenu.classList.remove('is-open'); // Предположим, что класс 'open' отвечает за видимость меню
+}
+
+// Добавляем обработчики событий клика к ссылкам в мобильном меню
+const menuLinks = document.querySelectorAll('#mobile-menu a');
+menuLinks.forEach(function (link) {
+  link.addEventListener('click', closeMobileMenu);
+});
+
 function handleTabletMediaQuery(event) {
   const mobileMenu = document.getElementById('mobile-menu');
 
-  //   if (event.matches) {
-  // Mobile view: screen width between 320px and 767px
   document.getElementById('menu-button').addEventListener('click', function () {
     mobileMenu.style.right = '0';
     overlay.classList.add('is-open');
@@ -20,31 +29,62 @@ function handleTabletMediaQuery(event) {
     .addEventListener('click', function () {
       mobileMenu.style.right = '-380px';
       overlay.classList.remove('is-open');
-      //   mobMenuWindow.classList.remove('is-open');
     });
 
   overlay.addEventListener('click', function () {
     mobileMenu.style.right = '-380px';
-    // mobMenuWindow.classList.remove('is-open');
     overlay.classList.remove('is-open');
   });
 
-  //   overlay.addEventListener('click', closeMobMenuWindow);
-  //   } else {
-  //     // Tablet view: screen width greater than or equal to 768px
-  //     document
-  //       .getElementById('menu-button')
-  //       .addEventListener('click', function () {
-  //         mobileMenu.style.right = '0';
-  //       });
-
-  //     document
-  //       .getElementById('close-button')
-  //       .addEventListener('click', function () {
-  //         mobileMenu.style.right = '-380px';
-  //       });
-  //   }
+  menuLinks.addEventListener('click', function () {
+    mobileMenu.style.right = '-380px';
+    overlay.classList.remove('is-open');
+  });
 }
 
-mediaQuery.addListener(handleTabletMediaQuery); // Attach the event listener
+// mediaQuery.addListener(handleTabletMediaQuery); // Attach the event listener
 handleTabletMediaQuery(mediaQuery); // Call the function once to check the initial state
+
+// // Функция для закрытия мобильного меню
+// function closeMobileMenu() {
+//   const mobileMenu = document.getElementById('mobile-menu');
+//   mobileMenu.classList.remove('is-open'); // Предположим, что класс 'open' отвечает за видимость меню
+// }
+
+// // Добавляем обработчики событий клика к ссылкам в мобильном меню
+// const menuLinks = document.querySelectorAll('#mobile-menu a');
+// menuLinks.forEach(function (link) {
+//   link.addEventListener('click', closeMobileMenu);
+// });
+
+// // Настройка медиа-запроса и его обработка
+// const mediaQuery = window.matchMedia(
+//   '(min-width: 320px) and (max-width: 767px)'
+// );
+// const overlay = document.querySelector('.backdrop');
+// const mobMenuWindow = document.querySelector('.mob-menu');
+
+// function handleTabletMediaQuery(event) {
+//   const mobileMenu = document.getElementById('mobile-menu');
+
+//   document.getElementById('menu-button').addEventListener('click', function () {
+//     mobileMenu.style.right = '0';
+//     overlay.classList.add('is-open');
+//     mobMenuWindow.classList.add('is-open');
+//   });
+
+//   document
+//     .getElementById('close-button')
+//     .addEventListener('click', function () {
+//       mobileMenu.style.right = '-380px';
+//       overlay.classList.remove('is-open');
+//     });
+
+//   overlay.addEventListener('click', function () {
+//     mobileMenu.style.right = '-380px';
+//     overlay.classList.remove('is-open');
+//   });
+// }
+
+// mediaQuery.addListener(handleTabletMediaQuery); // Привяжем обработчик события
+// handleTabletMediaQuery(mediaQuery); // Вызовем функцию один раз для проверки начального состояния
